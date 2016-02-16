832e65ee67080898e2662773392c07d73722a15a
Pete Katsaboulas <pete@virality.me>
2016-02-16 14:28:21 -0600

    add hover animations to buttons

--- js/main.js
+++ js/main.js
@@ -4,18 +4,22 @@ $(function(){
   $('.about-btn').on('click', function(){
       var about_top   = $('.about').offset().top;
       $('html,body').animate({ scrollTop: about_top + 'px' });
+      $(this).addClass('active').siblings().removeClass('active');
   });
   $('.race-btn').on('click', function(){
       var race_top   = $('.race').offset().top;
       $('html,body').animate({ scrollTop: race_top + 'px' });
+      $(this).addClass('active').siblings().removeClass('active');
   });
   $('.help-btn').on('click', function(){
       var help_top   = $('.help').offset().top;
       $('html,body').animate({ scrollTop: help_top + 'px' });
+      $(this).addClass('active').siblings().removeClass('active');
   });
   $('.contact-btn').on('click', function(){
       var contact_top   = $('footer').offset().top;
       $('html,body').animate({ scrollTop: contact_top + 'px' });
+      $(this).addClass('active').siblings().removeClass('active');
   });
 	
 	
